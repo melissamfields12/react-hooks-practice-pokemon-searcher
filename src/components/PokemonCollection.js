163ab2efc8d3c2b8b02e72ae26348ef6400bd9d2
Melissa Fields <melissamfields12@gmail.com>
2023-01-30 15:28:03 -0800

    commit tree a1132545862995805c93fb63c9aa6210068bafdc

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -2,10 +2,19 @@ import React from "react";
 import PokemonCard from "./PokemonCard";
 import { Card } from "semantic-ui-react";
 
-function PokemonCollection() {
+function PokemonCollection({pokemon}) {
+
+   const mappedPokemon = pokemon.map((poke) => {
+       return ( <PokemonCard 
+          key={poke.id}
+          pokemon={poke} 
+          />    
+       )
+})
+ 
   return (
     <Card.Group itemsPerRow={6}>
-      <h1>Hello From Pokemon Collection</h1>
+      {mappedPokemon}
     </Card.Group>
   );
 }
--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,19 +1,37 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
 
 function PokemonPage() {
+  const [pokemon, setPokemon] = useState([])
+  const [searchTerm, setSearchTerm] = useState('')
+
+  useEffect(() => {
+    fetch("http://localhost:3001/pokemon")
+    .then(resp => resp.json())
+    .then(data => setPokemon(data))
+  }, [])
+
+  const handleAddPokemon = (newPokemon) => {
+     setPokemon([newPokemon, ...pokemon])
+  }
+ 
+
+  const pokemonToDisplay = pokemon.filter(poke => 
+    poke.name.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
       <br />
-      <PokemonForm />
+      <PokemonForm onAddPokemon={handleAddPokemon}/>
       <br />
-      <Search />
+      <Search searchTerm={searchTerm} onChangeSearch={setSearchTerm}/>
       <br />
-      <PokemonCollection />
+      <PokemonCollection pokemon={pokemonToDisplay}/>
     </Container>
   );
 }
